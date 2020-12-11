cd6330cd24296ebd14a6a32b28a379ec538b3841
CMK-Aniki <74895786+CMK-Aniki@users.noreply.github.com>
2020-12-11 23:19:13 +0900

    Add files via upload

---
+++ 280getublock/280ublockCopy.py
@@ -0,0 +1,24 @@
+import datetime
+
+from selenium import webdriver
+import pyautogui
+
+dt_now_Year = datetime.datetime.now().year
+strYear =str(dt_now_Year)
+dt_now_Month = datetime.datetime.now().month
+strMonth=str(dt_now_Month)
+
+
+# ブラウザーを起動
+driver = webdriver.Chrome()
+
+driver.get('https://280blocker.net/files/280blocker_adblock_'+strYear+strMonth+'.txt')
+
+element = driver.find_element_by_tag_name("pre")
+
+pyautogui.keyDown('ctrl')
+pyautogui.press(['a', 'c'])
+pyautogui.keyUp('ctrl')
+
+# 終了処理
+driver.quit()
